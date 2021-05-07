c1185eee5a2d7e6f3e615e3cd8fa9bd88ce4d183
Miurell Cristina <miurellcristina@hotmail.es>
2021-05-06 18:29:51 -0700

    Agregar archivos de proyecto.

---
+++ Mayores100/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayores100
+{
+    class Program
+    {
+        private int[] vec;
+        public void Cargar()
+        {
+            Console.Write("Cantidad de elementos: ");
+            int n = int.Parse(Console.ReadLine());
+            vec = new int[n];
+            for (int f=0; f<vec.Length; f++)
+            {
+                Console.Write("Ingrese el valor del componente:  " );
+                String linea;
+                linea = Console.ReadLine();
+                vec[f] = int.Parse(linea);
+            }
+        }
+
+        public void imprimir()
+        {
+            int cant = 0;
+            foreach (var valor in vec)
+                if (valor >100)
+                {
+                    cant++;
+                }
+            Console.Write("La cantidad de elementos mayores a 100 son: " + cant);
+            Console.ReadKey();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program m = new Program();
+
+            m.Cargar();
+            m.imprimir();
+        }
+    }
+}
